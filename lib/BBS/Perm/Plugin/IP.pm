472e5a4ee1248c9ce60087032cd85f1f27a6583e
sunnavy <sunnavy@bestpractical.com>
2010-10-02 12:05:18 +0800

    update copyright

--- lib/BBS/Perm/Plugin/IP.pm
+++ lib/BBS/Perm/Plugin/IP.pm
@@ -165,7 +165,7 @@ sunnavy  C<< <sunnavy@gmail.com> >>
 
 =head1 LICENCE AND COPYRIGHT
 
-Copyright (c) 2007, sunnavy C<< <sunnavy@gmail.com> >>. All rights reserved.
+Copyright (c) 2007-2010, sunnavy C<< <sunnavy@gmail.com> >>. All rights reserved.
 
 This module is free software; you can redistribute it and/or
 modify it under the same terms as Perl itself. See L<perlartistic>.
